1e2ea97eaa860ad9130208ec1104c6bd2cad4543
choda20 <itaykadchoda@gmail.com>
2023-04-05 13:57:13 +0300

    removed boxes, needs testing

--- src/main/java/com/example/dotsandboxes/AI/AIBoard.java
+++ src/main/java/com/example/dotsandboxes/AI/AIBoard.java
@@ -17,7 +17,6 @@ public class AIBoard {
     private List<ModelLine> avlLines;
     private ModelLine[][] horizontalLines; // (gridSize)*(gridSize-1) matrix containing all the horizontal lines in the game
     private ModelLine[][] verticalLines; // (gridSize)*(gridSize-1) matrix containing all the vertical  lines in the game
-    private Box[][] boxes; // (gridSize-1)*(gridSize) matrix containing the boxes in the game
     private Random generator;
     private ModelLine lastMove;
 
@@ -37,7 +36,6 @@ public class AIBoard {
         this.generator = new Random();
 
         this.avlLines = new ArrayList<>();
-        this.boxes = new Box[gridSize-1][gridSize-1];
         this.horizontalLines = new ModelLine[gridSize][gridSize-1];
         this.verticalLines = new ModelLine[gridSize][gridSize-1];
         for (int i=0;i<gridSize;i++) {
@@ -50,27 +48,13 @@ public class AIBoard {
                     avlLines.add(verticalLines[i][j]);
             }
         }
-        for (int i=0;i<gridSize-1;i++) {
-            for (int j = 0; j < gridSize-1; j++) {
-                boxes[i][j] = new Box(new ArrayList<ModelLine>());
-                boxes[i][j].getLines().add(verticalLines[j][i]);
-                boxes[i][j].getLines().add(verticalLines[j+1][i]);
-                boxes[i][j].getLines().add(horizontalLines[i][j]);
-                boxes[i][j].getLines().add(horizontalLines[i+1][j]);
-                for (ModelLine line: boxes[i][j].getLines()) {
-                    if (line.isConnected())
-                        boxes[i][j].incConnectedLines();
-                }
-            }
-        }
+
     }
     public void setGridSize(int gridSize) {
-        this.boxes = new Box[gridSize-1][gridSize-1];
         this.horizontalLines = new ModelLine[gridSize][gridSize-1]; // array of horizontal lines
         this.verticalLines = new ModelLine[gridSize][gridSize-1]; // array of vertical lines
         this.avlLines = new ArrayList<ModelLine>();
         initializeLines(gridSize);
-        initializeBoxes(gridSize);
     }
     private void initializeLines(int gridSize) {
         for (int i=0;i<gridSize;i++) {
@@ -82,52 +66,27 @@ public class AIBoard {
             }
         }
     }
-    private void initializeBoxes(int gridSize) {
-        for (int i=0;i<gridSize-1;i++) {
-            for (int j = 0; j < gridSize-1; j++) {
-                boxes[i][j] = new Box(new ArrayList<ModelLine>());
-                boxes[i][j].getLines().add(verticalLines[j][i]);
-                boxes[i][j].getLines().add(verticalLines[j+1][i]);
-                boxes[i][j].getLines().add(horizontalLines[i][j]);
-                boxes[i][j].getLines().add(horizontalLines[i+1][j]);
-            }
-        }
-    }
+
     private int checkBoxFormed(ModelLine line) { // checks if a newly connected line completes boxes, and returns the points obtained from it(0-2)
-        List<Box> parentData = getParentBoxes(line);
-        int numberOfParents = parentData.size();
-        int score;
-        switch (numberOfParents) {
-            case 1:
-                parentData.get(0).incConnectedLines();
-                score = parentData.get(0).getIsComplete() ? 1 : 0;
-                return score;
-            case 2:
-                parentData.get(0).incConnectedLines();
-                parentData.get(1).incConnectedLines();
-                score = (parentData.get(0).getIsComplete() ? 1 : 0) + (parentData.get(1).getIsComplete() ? 1 : 0);
-                return score;
-            default:
-                return 0;
+        int x = line.getRow();
+        int y = line.getColumn();
+        int columnLength = horizontalLines[0].length;
+        int score = 0;
+        if (line.isHorizontal()) {
+            if (x>0 && x < columnLength && y < columnLength && horizontalLines[x-1][y].isConnected() && verticalLines[y][x-1].isConnected() && verticalLines[y+1][x-1].isConnected())
+                score += 1;
+            if (x < columnLength && y < columnLength && horizontalLines[x+1][y].isConnected() && verticalLines[y][x].isConnected() && verticalLines[y+1][x].isConnected())
+                score += 1;
         }
+        else {
+            if (x > 0 && x < columnLength && y < columnLength && verticalLines[x-1][y].isConnected() && horizontalLines[y][x-1].isConnected() && horizontalLines[y+1][x-1].isConnected())
+                score += 1;
+            if(x < columnLength && y < columnLength && verticalLines[x+1][y].isConnected() && horizontalLines[y][x].isConnected() && horizontalLines[y+1][x].isConnected())
+                score += 1;
+        }
+        return score;
     }
 
-    private List<Box> getParentBoxes(ModelLine line) { // returns a Box array containing the boxes a line is a part of, and an Integer containing the length of the array
-        List<Box> results = new ArrayList<>();
-        int resultIndex = 0;
-        for (int i = 0; i < getGridSize()-1; i++) {
-            for (int j = 0; j < getGridSize()-1; j++) {
-                if (boxes[i][j].hasLine(line)) {
-                    results.add(boxes[i][j]);
-                    resultIndex+=1;
-                    if (resultIndex >= 2) {
-                        return results;
-                    }
-                }
-            }
-        }
-        return results;
-    }
     public boolean isGameOngoing() { // returns true if the game is in progress, otherwise false
         boolean ongoing =  !((firstScore + secondScore) == ((getGridSize()-1)*(getGridSize()-1)));
         if (!ongoing) {
@@ -172,7 +131,6 @@ public class AIBoard {
         }
     }
     // getters
-    public Box[][] getBoxes() {return boxes;}
     public ModelLine[][] getHorizontalLines() {return horizontalLines;}
     public ModelLine[][] getVerticalLines() {return verticalLines;}
     public int getFirstScore() {return firstScore;}
